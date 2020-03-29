caeae1e3ead7d2f3f3d15c8ba2251b6150c10b34
Bunlong <bunlong.van@gmail.com>
2020-03-29 23:15:31 +0700

    Refactor.

--- demo/ReadRemoteFile.js
+++ demo/ReadRemoteFile.js
@@ -5,7 +5,7 @@ import { readRemoteFile } from 'react-papaparse'
 export default class ReadRemoteFile extends Component {
   handleClick = () => {
     readRemoteFile('http://example.com/file.csv', {
-      complete: function(results) {
+      complete: (results) => {
         console.log('Results:', results)
       }
     })
