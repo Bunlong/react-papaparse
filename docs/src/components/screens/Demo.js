6f0fcbf65b43900613f3f78a1f21dd25708f40a4
Bunlong <bunlong.van@gmail.com>
2020-05-01 00:35:59 +0700

    Fix preview.

--- docs/src/components/screens/Demo.js
+++ docs/src/components/screens/Demo.js
@@ -226,6 +226,9 @@ export default class Demo extends Component {
                           noClick
                           noDrag
                           onRemoveFile={this.handleOnRemoveFile}
+                          config={{
+                            preview: 1135
+                          }}
                         >
                           {({ file }) => (
                             <aside
@@ -293,6 +296,9 @@ export default class Demo extends Component {
                           onError={this.handleOnError}
                           addRemoveButton
                           onRemoveFile={this.handleOnRemoveFile}
+                          config={{
+                            preview: 100
+                          }}
                         >
                           <span>Drop CSV file here or click to upload.</span>
                         </CSVReader>
