0730fe69e5b44de6ebd8cc425bcbecbc166cae96
Bunlong <bunlong.van@gmail.com>
2021-09-08 22:17:55 +0700

    Define jsonToCSV in usePapaParse

--- src/usePapaParse.tsx
+++ src/usePapaParse.tsx
@@ -1,9 +1,11 @@
 import { readString } from './readString';
 import { readRemoteFile } from './readRemoteFile';
+import { jsonToCSV } from './jsonToCSV';
 
 export function usePapaParse() {
   return {
     readString,
     readRemoteFile,
+    jsonToCSV,
   };
 }
