10427222d000a0c2ef972e009db1d7e9ff76e2b3
Bunlong <bunlong.van@gmail.com>
2021-11-04 11:24:41 +0700

    Add reference

--- src/CSVReader.tsx
+++ src/CSVReader.tsx
@@ -84,7 +84,8 @@ const styles = {
   } as CSSProperties,
 };
 
-// https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/papaparse/index.d.ts
+// 5.3 => https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/papaparse/index.d.ts
+// 5.2 => https://github.com/DefinitelyTyped/DefinitelyTyped/blob/d3737ebd9125505f7ea237b9f17f1426579a3917/types/papaparse/index.d.ts
 interface CustomConfig<T = any, TInput = undefined> extends ParseConfig<T, TInput> {
   /** The encoding to use when opening local files. If specified, it must be a value supported by the FileReader API. */
   encoding?: string | undefined;
