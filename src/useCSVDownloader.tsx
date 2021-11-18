a302eee5baafa3a3333d7a4bc88e5d8ba75a7fcc
Bunlong <bunlong.van@gmail.com>
2021-11-18 23:44:21 +0700

    Create useCSVDownloader

---
+++ src/useCSVDownloader.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PapaParse, { UnparseConfig } from 'papaparse';
+
+export interface Props {
+
+}
+
+export interface Api {
+  
+}
+
+function useCSVDownloaderComponent(api: Api) {
+
+}
+
+export function useCSVDownloader() {
+
+}
